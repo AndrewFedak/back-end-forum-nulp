9f9406e5a2942f13cc4d6f3f10ea298361aa6cd8
riba12331 <fedakandrew@gmail.com>
2020-05-24 15:52:17 +0300

    added

--- src/express/app.js
+++ src/express/app.js
@@ -4,7 +4,10 @@ const articlesRouter = require('./router/articles');
 const usersRouter = require('./router/users');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 
+
+app.use(express.static(path.join(__dirname, '../../../build')));
 app.use(express.json());
 app.use(cors());
 app.use(articlesRouter);
