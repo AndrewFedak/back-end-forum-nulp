688d93d71ef594d50ca5fe0b9428dafe9470b741
riba12331 <fedakandrew@gmail.com>
2020-05-24 15:55:26 +0300

    removed

--- src/express/app.js
+++ src/express/app.js
@@ -4,10 +4,8 @@ const articlesRouter = require('./router/articles');
 const usersRouter = require('./router/users');
 const cors = require('cors');
 const app = express();
-const path = require('path');
 
 
-app.use(express.static(path.join(__dirname, '../../../build')));
 app.use(express.json());
 app.use(cors());
 app.use(articlesRouter);
