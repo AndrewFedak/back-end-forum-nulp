352a4d935d905ed5ee2e05fa1ac21b6d24e739d5
Andrew <fedakandrew@gmail.com>
2021-09-18 00:33:17 +0300

    added pagination

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const MONGO_URL = process.env.MONGODB_URL
+const MONGO_URL = process.env.MONGODB_URL || "mongodb+srv://admin:Dubau2002@forumcluster.uok4s.mongodb.net/forum-db?retryWrites=true&w=majority"
 mongoose.connect(MONGO_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
--- src/express/app.js
+++ src/express/app.js
@@ -5,14 +5,10 @@ const usersRouter = require('./router/users');
 const cors = require('cors');
 const app = express();
 
-
+const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 app.use(articlesRouter);
 app.use(usersRouter);
 
-app.get('/', async (req, res) => {
-    res.status(200).send('Andrew Fedak')
-});
-
-app.listen(process.env.PORT, () => console.log('We are on ' + process.env.PORT + ' port, db ' + process.env.MONGODB_URL));
+app.listen(port, () => console.log('We are on ' + port + ' port, db ' + process.env.MONGODB_URL));
