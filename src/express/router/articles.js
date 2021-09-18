38e9aafa3dd23fb3576672743132721548b3e000
Andrew <fedakandrew@gmail.com>
2021-09-18 13:40:51 +0300

    changed pagination oprions

--- src/express/router/articles.js
+++ src/express/router/articles.js
@@ -6,21 +6,27 @@ const auth = require('../middleware/auth');
 router.get('/api/articles', async (req, res) => {
     const {page, limit = 9, searchValue = ''} = req.query;
     try {
-        const [amount, articles] = await Promise.all(
-            [Articles.countDocuments({}),
+        const [allArticles, paginatedArticles] = await Promise.all(
+            [Articles.find({}),
             Articles
                 .find({})
                 .skip((page-1)*limit)
                 .limit(+limit)
             ])
-        const filteredArray = (function() {
-            if (searchValue === '') return articles;
-            return articles.filter((article) => {
-                const {articleText} = article.particularArticle
-                return articleText.indexOf(searchValue) > -1;
+        const filterBySearch = (data) => {
+            if (searchValue === '') return data;
+            return data.filter((article) => {
+                const {articleText = ''} = article.particularArticle
+                const {shortDescription = ''} = article.articlePreview;
+                return articleText.indexOf(searchValue) > -1
+                    || shortDescription.indexOf(searchValue) > -1
+                    || article.titleTheme.indexOf(searchValue) > -1
             });
-        }());
-        res.status(200).send({amount, articles: filteredArray})
+        }
+        res.status(200).send({
+            amount: filterBySearch(allArticles).length,
+            articles: filterBySearch(paginatedArticles)
+        })
     } catch (e) {
         res.status(500).send();
     }
